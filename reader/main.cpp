50ec02d20dda604ce838e86758ad8a1dd32ce7ff
Guillaume Simard <guillaume.simard@cephei.ca>
2012-02-18 20:14:06 -0500

    Debug: istringstream.

--- reader/main.cpp
+++ reader/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "anvil.hpp"
 using namespace std;
 
 
 int main( int argc, char* argv[] )
 {
+    byte *data = new byte[4];
+    data[0] = 0;
+    data[1] = 0;
+    data[2] = 1;
+    data[3] = 0;
+
+    byte *data2 = new byte[4];
+
+    string m_string = string( (char*)data, 4 );
+    istringstream ss( m_string, ios_base::binary | ios_base::in );
+
+    unsigned long int m_int;
+    ss.read( (char*)data2, 4 );
+    m_int = *(unsigned long int*)data2;
+    cout << m_int << endl;
+
+    delete data;
+    delete data2;
+    return 0;
+
     if (argc != 2)
     {
         cout << "Usage: reader file" << endl;
@@ -24,9 +45,14 @@ int main( int argc, char* argv[] )
 
     for ( int i = 0 ; i < 1024 ; i++ )
     {
-        if (anvil.chunks[i].id.offset != 0)
+        if (anvil.chunks[i].data != NULL)
         {
+            ofstream mc_chunk_file;
+            mc_chunk_file.open( "chunk.dat", ios::binary );
+            mc_chunk_file.write( (char*)anvil.chunks[i].data, anvil.chunks[i].length );
+            mc_chunk_file.close();
 
+            break;
         }
     }
 
